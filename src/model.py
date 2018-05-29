58f799a735900d34f103a18afdb8b1e696c37508
Sayan Goswami <goswami.sayan47@gmail.com>
2018-05-29 18:35:25 +0530

    encoder done

--- src/model.py
+++ src/model.py
@@ -3,9 +3,10 @@ import torch.nn as nn
 
 
 class SegNet(nn.Module):
-    def __init__(self, **kwargs):
+    def __init__(self, batch_norm=True):
         super(SegNet, self).__init__()
 
+        self.num_channels = 3
         self.features = self.encoder() + self.decoder()
 
         self.model = nn.Sequential(*self.features)
@@ -15,33 +16,99 @@ class SegNet(nn.Module):
         pass
 
     
-    def encoder(self, batch_norm=False):
+    def encoder(self):
         """Construct VGG-16 network"""
         layers = []
-        num_channels = 3
-
+        
         # int - filter dim, 'M' - max pool
         vgg16_dims = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
 
-        for dim in vgg16_dims:
-            if dim == 'M':
-                layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
-            
-            else:
-                layers.append(nn.Conv2d(num_channels, dim, kernel_size=3, padding=1))
+        # Stage 1
+        
+        layers.append(nn.Conv2d(3, 64, kernel_size=3, padding=1))
+        layers.append(nn.BatchNorm2d(64))
+        layers.append(nn.ReLU(inplace=True))
 
-                if batch_norm:
-                    layers.append(nn.BatchNorm2d(dim))
+        layers.append(nn.Conv2d(64, 64, kernel_size=3, padding=1))
+        layers.append(nn.BatchNorm2d(64))
+        layers.append(nn.ReLU(inplace=True))
 
-                layers.append(nn.ReLU(inplace=True))
+        layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
 
-                num_channels = dim
+
+        # Stage 2
+
+        layers.append(nn.Conv2d(64, 128, kernel_size=3, padding=1))
+        layers.append(nn.BatchNorm2d(128))
+        layers.append(nn.ReLU(inplace=True))
+
+        layers.append(nn.Conv2d(128, 128, kernel_size=3, padding=1))
+        layers.append(nn.BatchNorm2d(128))
+        layers.append(nn.ReLU(inplace=True))
+
+        layers.append(nn.MaxPool2d(kernel_size256=2, stride=2))
+
+
+        # Stage 3
+
+        layers.append(nn.Conv2d(128, 256, kernel_size=3, padding=1))
+        layers.append(nn.BatchNorm2d(256))
+        layers.append(nn.ReLU(inplace=True))
+
+        layers.append(nn.Conv2d(256, 256, kernel_size=3, padding=1))
+        layers.append(nn.BatchNorm2d(256))
+        layers.append(nn.ReLU(inplace=True))
+
+        layers.append(nn.Conv2d(256, 256, kernel_size=3, padding=1))
+        layers.append(nn.BatchNorm2d(256))
+        layers.append(nn.ReLU(inplace=True))
+
+        layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
+
+
+
+        # Stage 4
+
+        layers.append(nn.Conv2d(256, 512, kernel_size=3, padding=1))
+        layers.append(nn.BatchNorm2d(512))
+        layers.append(nn.ReLU(inplace=True))
+
+        layers.append(nn.Conv2d(512, 512, kernel_size=3, padding=1))
+        layers.append(nn.BatchNorm2d(512))
+        layers.append(nn.ReLU(inplace=True))
+
+        layers.append(nn.Conv2d(512, 512, kernel_size=3, padding=1))
+        layers.append(nn.BatchNorm2d(512))
+        layers.append(nn.ReLU(inplace=True))
+
+        layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
+
+
+        # Stage 5
+
+        layers.append(nn.Conv2d(256, 512, kernel_size=3, padding=1))
+        layers.append(nn.BatchNorm2d(512))
+        layers.append(nn.ReLU(inplace=True))
+
+        layers.append(nn.Conv2d(512, 512, kernel_size=3, padding=1))
+        layers.append(nn.BatchNorm2d(512))
+        layers.append(nn.ReLU(inplace=True))
+
+        layers.append(nn.Conv2d(512, 512, kernel_size=3, padding=1))
+        layers.append(nn.BatchNorm2d(512))
+        layers.append(nn.ReLU(inplace=True))
+
+        layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
 
         return layers
 
 
-    def decoder(self, batch_norm=False):
-        pass
+    def decoder(self):
+        """Decoder part of SegNet"""
+        layers = []
+
+        decoder_dims = []
+
 
 
 
