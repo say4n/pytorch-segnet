21be4b0a2c8cd4099f3d951b961caeeefaf1d145
Sayan Goswami <goswami.sayan47@gmail.com>
2018-05-31 14:34:44 +0530

    training works!

--- src/train.py
+++ src/train.py
@@ -23,29 +23,49 @@ CUDA = False
 
 
 data_root = os.path.join("data", "VOCdevkit", "VOC2007")
-train_path = os.path.join(data_root, "ImageSets", "Segmentation", "train.txt")
+train_path = os.path.join(data_root,"ImageSets", "Segmentation", "train.txt")
 val_path = os.path.join(data_root, "ImageSets", "Segmentation", "val.txt")
 img_dir = os.path.join(data_root, "JPEGImages")
 mask_dir = os.path.join(data_root, "SegmentationObject")
 
 image_transform = transforms.ToTensor()
 
-train_dataset = PascalVOCDataset(list_file=train_path, img_dir=img_dir, mask_dir=mask_dir, transform=image_transform)
-val_dataset = PascalVOCDataset(list_file=val_path, img_dir=img_dir, mask_dir=mask_dir, transform=image_transform)
 
-train_dataloader = DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=4)
-val_data = DataLoader(val_dataset, batch_size=4, shuffle=True, num_workers=4)
+train_dataset = PascalVOCDataset(list_file=train_path,
+                                 img_dir=img_dir,
+                                 mask_dir=mask_dir,
+                                 transform=image_transform)
+
+train_dataloader = DataLoader(train_dataset,
+                              batch_size=1,
+                              shuffle=True, 
+                              num_workers=4)
+
+
+val_dataset = PascalVOCDataset(list_file=val_path,
+                               img_dir=img_dir,
+                               mask_dir=mask_dir,
+                               transform=image_transform)
+
+val_data = DataLoader(val_dataset,
+                      batch_size=1,
+                      shuffle=True,
+                      num_workers=4)
 
 
 
 
 if CUDA:
-    model = SegNet(input_channels=NUM_INPUT_CHANNELS, output_channels=NUM_OUTPUT_CHANNELS).cuda()
+    model = SegNet(input_channels=NUM_INPUT_CHANNELS,
+                   output_channels=NUM_OUTPUT_CHANNELS).cuda()
 else:
-    model = SegNet(input_channels=NUM_INPUT_CHANNELS, output_channels=NUM_OUTPUT_CHANNELS)
+    model = SegNet(input_channels=NUM_INPUT_CHANNELS,
+                   output_channels=NUM_OUTPUT_CHANNELS)
 
 criterion = torch.nn.MSELoss()
-optimiser = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE, momentum=MOMENTUM)
+optimizer = torch.optim.SGD(model.parameters(),
+                            lr=LEARNING_RATE, 
+                            momentum=MOMENTUM)
 
 
 
@@ -53,14 +73,12 @@ model.train()
 
 for epoch in range(NUM_EPOCHS):
     for i_batch, batch in enumerate(train_dataset):
-        print(batch['image'].size(), batch['mask'].size())
-
         t_start = time.time()
 
-        input_tensor = torch.autograd.Variable(batch['image'])
-        target_tensor = torch.autograd.Variable(batch['mask'])
+        input_tensor = torch.autograd.Variable(batch['image'].view((1,3,224,224)))
+        target_tensor = torch.autograd.Variable(batch['mask'].view((1,1,224,224)))
 
-        predicted_tensor = model(input_tensor)
+        predicted_tensor, softmaxed_tensor = model(input_tensor)
 
         loss = criterion(predicted_tensor, target_tensor)
 
@@ -73,7 +91,4 @@ for epoch in range(NUM_EPOCHS):
         
         delta = time.time() - t_start
 
-        print(f"""Epoch #{i+1}
-              Loss = {loss_f}
-              Time = {delta}secs
-              """)
+        print("Epoch #{}\tTime = {:.2f} s\nLoss = {:.6f}".format(i_batch+1, loss_f, delta))
