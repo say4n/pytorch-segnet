b9013c1bb10c946743106f5ad07bfa2ee4e3c59c
Sayan Goswami <goswami.sayan47@gmail.com>
2018-06-01 18:47:12 +0530

    np

--- src/inference.py
+++ src/inference.py
@@ -71,15 +71,18 @@ def validate():
             fig = plt.figure()
 
             a = fig.add_subplot(1,3,1)
-            plt.imshow(np.transpose(input_image.detach().cpu().numpy().reshape(3, 224, 224), (1,2,0)))
+            input_imx = input_image.detach().cpu().numpy().reshape(3, 224, 224)
+            plt.imshow(np.transpose(input_imx, (1,2,0)))
             a.set_title('Input Image')
             
             a = fig.add_subplot(1,3,2)
-            plt.imshow(np.transpose(predicted_mask.detach().cpu().numpy().reshape(3, 224, 224), (1,2,0)))
+            predicted_mx = predicted_mask.detach().cpu().numpy().reshape(3, 224, 224)
+            plt.imshow(np.transpose(predicted_mx, (1,2,0)))
             a.set_title('Predicted Mask')
 
             a = fig.add_subplot(1,3,3)
-            plt.imshow(np.transpose(target_mask.detach().cpu().numpy().reshape(3, 224, 224), (1,2,0)))
+            target_mx = target_mask.detach().cpu().numpy().reshape(3, 224, 224)
+            plt.imshow(np.transpose(target_mx, (1,2,0)))
             a.set_title('Ground Truth')
 
             fig.savefig(os.path.join(OUTPUT_DIR, "prediction_{}_{}.png".format(batch_idx, idx)))
